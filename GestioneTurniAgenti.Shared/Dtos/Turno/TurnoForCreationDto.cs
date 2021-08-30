3524fb0b7fd2db4bc3752b0b0cfc501e78b87a25
Romeo di Gregorio <r.digregorio@crif.com>
2021-08-30 16:29:53 +0200

    Add Data Annotation on dtos.

--- GestioneTurniAgenti.Shared/Dtos/Turno/TurnoForCreationDto.cs
+++ GestioneTurniAgenti.Shared/Dtos/Turno/TurnoForCreationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace GestioneTurniAgenti.Shared.Dtos.Turno
 {
     public class TurnoForCreationDto
     {
+        [Required]
         public Guid AgenteId { get; set; }
+
+        [Required(ErrorMessage = "Valore obbligatorio.")]
         public Guid EventoId { get; set; }
+
+        [Required(ErrorMessage = "Valore obbligatorio.")]
         public DateTime Data { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Lunghezza massima di 500 caratteri.")]
         public string Note { get; set; }
     }
 }
--- GestioneTurniAgenti.Shared/Dtos/Turno/TurnoForUpdateDto.cs
+++ GestioneTurniAgenti.Shared/Dtos/Turno/TurnoForUpdateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace GestioneTurniAgenti.Shared.Dtos.Turno
 {
     public class TurnoForUpdateDto
     {
+        [Required]
         public Guid AgenteId { get; set; }
+
+        [Required]
         public Guid EventoId { get; set; }
+
+        [Required]
         public DateTime Data { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Lunghezza massima di 500 caratteri.")]
         public string Note { get; set; }
     }
 }
