0ca2ae53c47519570e6e66d35a4ce4515874817f
Romeo di Gregorio <r.digregorio@crif.com>
2021-08-31 12:18:10 +0200

    Implements Delete with confirmation modal.

---
+++ GestioneTurniAgenti.Client/Components/ConfirmationDeleteTurno.razor.cs
@@ -0,0 +1,36 @@
+﻿using GestioneTurniAgenti.Shared.Dtos.Turno;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneTurniAgenti.Client.Components
+{
+    public partial class ConfirmationDeleteTurno
+    {
+        private string _modalDisplay;
+        private bool _showBackdrop;
+
+        public TurnoDto TurnoToShow { get; set; } = new();
+
+        [Parameter]
+        public EventCallback OnOKClicked { get; set; }
+
+        public void Show(TurnoDto turnoDto)
+        {
+            TurnoToShow = turnoDto;
+            _modalDisplay = "block;";
+            _showBackdrop = true;
+            StateHasChanged();
+        }
+
+        public void Hide()
+        {
+            _modalDisplay = "none;";
+            _showBackdrop = false;
+            StateHasChanged();
+        }
+    }
+}
--- GestioneTurniAgenti.Client/Pages/Turni.razor.cs
+++ GestioneTurniAgenti.Client/Pages/Turni.razor.cs
@@ -1,4 +1,5 @@
-﻿using GestioneTurniAgenti.Client.Components;
+﻿using Blazored.Toast.Services;
+using GestioneTurniAgenti.Client.Components;
 using GestioneTurniAgenti.Client.HttpInterceptor;
 using GestioneTurniAgenti.Client.Services;
 using GestioneTurniAgenti.Shared.Dtos.Anagrafica;
@@ -26,7 +27,13 @@ namespace GestioneTurniAgenti.Client.Pages
         public ITurniService TurniService { get; set; }
 
         [Inject]
+        public IToastService ToastService { get; set; }
+
+        [Inject]
         public HttpInterceptorService Interceptor { get; set; }
+
+        private ConfirmationDeleteTurno _confirmation;
+        private Guid _turnoToDeleteId;
 
         public List<RepartoDto> Reparti { get; set; } = new();
         public List<EventoDto> Eventi { get; set; } = new();
@@ -55,6 +62,20 @@ namespace GestioneTurniAgenti.Client.Pages
         {
             TurniReturned = await TurniService.GetAllTurni(TurniSearchParameters);
             StateHasChanged();
+        }
+
+        public void CallConfirmationModal(TurnoDto turnoToDeleteDto)
+        {
+            _confirmation.Show(turnoToDeleteDto);
+            _turnoToDeleteId = turnoToDeleteDto.Id;
+        }
+
+        public async Task DeleteTurno()
+        {
+            _confirmation.Hide();
+            await TurniService.DeleteTurno(_turnoToDeleteId);
+            ToastService.ShowSuccess("Turno eliminato con successo!");
+            await GetTurni();
         }
 
         public void Dispose()
